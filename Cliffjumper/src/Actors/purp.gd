c8f92a9e768942aac7896e7f319cd0630365860b
Reuben R Atendido <rbendido178@gmail.com>
2022-06-09 20:16:51 +0100

    Add files via upload
    Added player movement for left, right and jump, added enemy left and right movement, added player and enemy sprites, added sprite animations, added collectable item texture, added stomp attack for player, added health attribute for actor types, added many varying test maps, added galaxy background.

--- Cliffjumper/src/Actors/purp.gd
+++ Cliffjumper/src/Actors/purp.gd
@@ -1,2 +1,63 @@
+class_name Purp
 extends Actor
+
+export var headshot_impulse: = 5000.0
+var HEALTH = 10
+
+onready var sprite = $SpiderPurpAni
+
+func _on_EnemyDetector_area_entered(area: Area2D) -> void:
+	velocity = calculate_headshot_velocity(velocity, headshot_impulse)
+	
+func _on_EnemyDetector_body_entered(body: PhysicsBody2D) -> void:
+	if HEALTH > 1:
+		HEALTH -= 1
+	else:
+		queue_free()
+	
+func _physics_process(delta: float) -> void:
+	var is_jump_interrupted: = Input.is_action_just_released("jump") and velocity.y < 0.0
+	var direction: = get_direction()
+	
+	if direction.x != 0:
+		if direction.x > 0:
+			sprite.scale.x = -0.119
+		else:
+			sprite.scale.x = 0.119
+
+	velocity = calculate_move_velocity(velocity, direction, speed, is_jump_interrupted)
+	velocity = move_and_slide(velocity, FLOOR_NORMAL)
+	
+	
+func get_direction() -> Vector2:
+	return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), 
+		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 0.0
+	)
+
+func calculate_move_velocity(
+		linear_velocity: Vector2,
+		direction: Vector2,
+		speed: Vector2,
+		is_jump_interrupted: bool
+	) -> Vector2:
+	var out: = linear_velocity
+	out.x = speed.x * direction.x
+	out.y += gravity * get_physics_process_delta_time()			
+	if direction.y == -1.0:
+		out.y = speed.y * direction.y
+	if is_jump_interrupted:
+		out.y = 0.0
+	return out
+
+func calculate_headshot_velocity(linear_velocity: Vector2, impulse: float) -> Vector2:
+	var out: = linear_velocity
+	out.y = -impulse
+	return out
+
+
+
+
+
+
 
